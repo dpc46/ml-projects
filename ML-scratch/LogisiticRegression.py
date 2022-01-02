18a87daa3b3d84c1aaee4b6fa423d437eadc77c6
dcp46 <danpcochrane@gmail.com>
2022-01-02 19:23:33 +0000

    remove excess spaces

--- ML-scratch/LogisiticRegression.py
+++ ML-scratch/LogisiticRegression.py
@@ -16,7 +16,7 @@ class LogisticRegression:
 
         #gradient descent
         for _ in range(self.n_iters):
-            
+
             linear_model = np.dot(X, self.weights) + self.bias
             y_predicted = self._sigmoid(linear_model)
 
@@ -25,17 +25,14 @@ class LogisticRegression:
 
             self.weights -= self.lr * dw
             self.bias -= self.lr * db
-        
 
     def predict(self, X):
-            
-            linear_model = np.dot(X, self.weights) + self.bias
-            y_predicted = self._sigmoid(linear_model)
-            y_predicted_classes  = [1 if i > 0.5 else 0 for i in y_predicted]
-            return y_predicted_classes
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = self._sigmoid(linear_model)
+        y_predicted_classes  = [1 if i > 0.5 else 0 for i in y_predicted]
+        return y_predicted_classes
 
     def _sigmoid(self, x):
         return 1/(1 + np.exp(-x))
-
 
     
